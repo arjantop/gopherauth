e51c44bbf0f1a4e363a466f6c0fab2c5eaa20f25
Arjan Topolovec <arjan.top@gmail.com>
2014-07-23 21:47:27 +0200

    Added user authentication service for integration testing

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/arjantop/gopherauth/login"
@@ -11,9 +12,30 @@ import (
 	"github.com/arjantop/gopherauth/util"
 )
 
+type UserAuthenticationServiceTest struct {
+	sessionMap map[string]string
+}
+
+func (m *UserAuthenticationServiceTest) IsSessionValid(sessionId string) (bool, error) {
+	return m.sessionMap[sessionId] != "", nil
+}
+
+func (m *UserAuthenticationServiceTest) AuthenticateUser(user, password string) (string, error) {
+	if user == "user1@example.com" && password == "pass1" {
+		m.sessionMap["session1"] = "user1@example.com"
+		return "session1", nil
+	} else if user == "user1@example.com" {
+		return "", service.CredentialsMismatch{}
+	} else {
+		return "", errors.New("error")
+	}
+}
+
 func main() {
 	serverKey := []byte("server_key")
 	tokenGenerator := service.NewCryptoTokenGenerator()
+
+	userAuthService := &UserAuthenticationServiceTest{}
 
 	templateFactory := util.NewTemplateFactory("templates")
 	http.Handle("/token", endpoint.NewTokenEndpointHandler(nil))
@@ -33,7 +55,7 @@ func main() {
 	approvalHandler := endpoint.NewApprovalEndpointHandler(nil, nil, nil, nil)
 	http.Handle("/approval", approvalHandler)
 
-	loginHandler := login.NewLoginHandler(serverKey, nil, tokenGenerator, templateFactory)
+	loginHandler := login.NewLoginHandler(serverKey, userAuthService, tokenGenerator, templateFactory)
 	http.Handle("/login", loginHandler)
 
 	http.ListenAndServe(":3000", nil)
